2239aa44b54cd28c7a650598e8b31cf40faf80b9
chronicle20 <a.chronicle.20@gmail.com>
2025-02-24 19:33:48 -0500

    Add templates grid.

---
+++ app/templates/columns.tsx
@@ -0,0 +1,59 @@
+"use client"
+
+import { ColumnDef } from "@tanstack/react-table"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger
+} from "@/components/ui/dropdown-menu";
+import {Button} from "@/components/ui/button";
+import {MoreHorizontal} from "lucide-react";
+
+// This type is used to define the shape of our data.
+// You can use a Zod schema here if you want.
+export type Template = {
+    id: string
+    attributes: {
+        region: string
+        majorVersion: number
+        minorVersion: number
+    }
+}
+
+export const columns: ColumnDef<Template>[] = [
+    {
+        accessorKey: "attributes.region",
+        header: "Region",
+    },
+    {
+        accessorKey: "attributes.majorVersion",
+        header: "Major",
+    },
+    {
+        accessorKey: "attributes.minorVersion",
+        header: "Minor",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+            return (
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" className="h-8 w-8 p-0">
+                            <span className="sr-only">Open menu</span>
+                            <MoreHorizontal className="h-4 w-4" />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem>
+                            <a href={"/templates/" + row.getValue("id") + "/properties"}>
+                                <span>View Template</span>
+                            </a>
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            )
+        },
+    },
+]
---
+++ app/templates/page.tsx
@@ -0,0 +1,109 @@
+"use client"
+
+import {useTenant} from "@/context/tenant-context";
+import {DataTable} from "@/components/data-table";
+import {columns} from "@/app/templates/columns";
+import {useEffect, useState} from "react";
+
+interface Template {
+    id: string;
+    attributes: {
+        region: string;
+        majorVersion: number;
+        minorVersion: number;
+        characters: {
+            templates: {
+                jobIndex: number;
+                subJobIndex: number;
+                gender: number;
+                mapId: number;
+                faces: number[];
+                hairs: number[];
+                hairColors: number[];
+                skinColors: number[];
+                tops: number[];
+                bottoms: number[];
+                shoes: number[];
+                weapons: number[];
+                items: number[];
+                skills: number[];
+            }[];
+        };
+        npcs: {
+            npcId: number;
+            impl: string;
+        }[];
+        socket: {
+            handlers: {
+                opCode: string;
+                validator: string;
+                handler: string;
+                options: any;
+            }[];
+            writers: {
+                opCode: string;
+                writer: string;
+                options: any;
+            }[];
+        }
+        worlds: {
+            name: string;
+            flag: string;
+            serverMessage: string;
+            eventMessage: string;
+            whyAmIRecommended: string;
+        }[];
+    };
+}
+
+export default function Page() {
+    const {activeTenant} = useTenant();
+    const [templates, setTemplates] = useState<Template[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!activeTenant) return;
+
+        const fetchTemplates = async () => {
+            try {
+                const rootUrl = process.env.NEXT_PUBLIC_ROOT_API_URL || "http://localhost:3000";
+                console.log(activeTenant?.id);
+                const response = await fetch(rootUrl + "/api/configurations/templates", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                        "TENANT_ID": activeTenant?.id,
+                        "REGION": activeTenant?.attributes.region,
+                        "MAJOR_VERSION": activeTenant?.attributes.majorVersion,
+                        "MINOR_VERSION": activeTenant?.attributes.minorVersion,
+                    },
+                });
+                if (!response.ok) {
+                    throw new Error("Failed to fetch templates.");
+                }
+                const data = await response.json();
+                setTemplates(data.data);
+            } catch (err: any) {
+                setError(err.message);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchTemplates();
+    }, [activeTenant]);
+
+    return (
+        <div className="flex flex-col flex-1 container mx-auto p-5 h-full">
+            <div className="items-center justify-between space-y-2">
+                <div>
+                    <h2 className="text-2xl font-bold tracking-tight">Templates</h2>
+                </div>
+            </div>
+            <div className="mt-4">
+                <DataTable columns={columns} data={templates}/>
+            </div>
+        </div>
+    );
+}
